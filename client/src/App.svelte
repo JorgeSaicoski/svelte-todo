4825c3242fd990367097e4642835e3f829d4128d
Jorge Sarkis <86751142+JorgeSaicoski@users.noreply.github.com>
2023-01-29 16:17:06 -0300

    add todo

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,14 +1,23 @@
 <script>
-
+	import {todoList} from "./store";
 	import Form from "./components/Form.svelte";
 	import Card from "./components/Card.svelte";
 
-	export let todoList
+	export let initTodoList
 	export let user
 
+	todoList.subscribe(e=>{
+		console.log(e)
+	})
 	function addTodo(e){
-		todoList.push(e.detail)
-		console.log(todoList)
+		initTodoList.push(e.detail)
+		/*
+		todoList.update(todo =>{
+			todo.push(e.detail)
+		})
+		
+		 */
+		todoList.set(initTodoList)
 	}
 
 
@@ -17,7 +26,7 @@
 <main>
 	<h1>Hello {user.name}! </h1>
 	<Form on:new={addTodo}></Form>
-	{#each todoList as todo, index}
+	{#each $todoList as todo, index}
 		<Card {todo}></Card>
 	{/each}
 </main>
